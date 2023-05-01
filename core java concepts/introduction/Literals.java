933c95bb8c88b75062cc642bb94f64064db8e8d3
Binayak Purohit <binayakpurohit007@gmail.com>
2023-05-01 20:45:24 +0530

    added Literals

---
+++ core java concepts/introduction/Literals.java
@@ -0,0 +1,25 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+
+        boolean a = true;
+        System.out.println("This is a byte literal- "+age);
+        System.out.println("This is a int literal- "+age2);
+        System.out.println("This is a short literal- "+age3);
+        System.out.println("This is a long literal- "+ageDino);
+        System.out.println("This is a char literal- "+ch);
+        System.out.println("This is a float literal- "+f1);
+        System.out.println("This is a double literal- "+d1);
+        System.out.println("This is a boolean literal- "+a);
+
+        String str = "Binayak";
+        System.out.println("This is a String literal- "+str);
+
+    }
+}
