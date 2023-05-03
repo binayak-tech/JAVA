80d83a9b7b8d970c4b6a5b9f547361dd6a6696d5
Binayak Purohit <binayakpurohit007@gmail.com>
2023-05-04 03:29:44 +0530

    arrays concept added

--- core_java_concepts/arrays/MultiDimensionalArrays.java
+++ core_java_concepts/arrays/MultiDimensionalArrays.java
@@ -1,5 +1,55 @@
+/*  Multi-Dimensional arrays are the arrays where each element is also an array
+        [[],[],[]], 
+        [[],[],[]],
+        [[],[],[]]
+        This is a two dimensional array- it can be shown in the form of a matrix.
+        example - two dimensional -->  int[][] arr = new int[][];
+        example - three dimensional --> int[][][] arr = new int[][][];
+*/
+
 package arrays;
 
+import java.util.Scanner;
+
 public class MultiDimensionalArrays {
-    
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number of rows: ");
+        int row = scan.nextInt();
+        System.out.print("\nEnter number of columns: ");
+        int col = scan.nextInt();
+
+
+        int[][] arr = enterArray(scan, row, col);
+        printMatrix(arr, row, col);
+    }
+
+    // initialization
+    public static int[][] enterArray(Scanner scan,int row, int col){
+        int[][] arr = new int[row][col];
+        System.out.println("Enter 9 inputs for a two matrix of 3x3");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; col++){
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        return arr;
+    }
+  
+    // display
+     public static void printMatrix(int[][] matrix, int row, int col){
+       System.out.println("Your Matrix is : ");
+           
+           for (int i = 0; i < row; i++)
+           {
+               for (int j = 0; j < col; j++)
+               {
+                   System.out.print(matrix[i][j]+"\t");
+               }
+                
+               System.out.println();
+           }
+     }
 }
--- core_java_concepts/arrays/OneDimensionalArrays.java
+++ core_java_concepts/arrays/OneDimensionalArrays.java
@@ -1,5 +1,45 @@
 package arrays;
 
 public class OneDimensionalArrays {
-    
+    // Array is a collection of homogeneous elements.   
+    // syntax - type [] arr_name = {ele1,ele2,ele3...};
+    // syntax - type [] arr_name = new type[size];
+    // accessing array --> arr_name[index];
+    // initializing array --> arr_name[index] = value;
+
+    public static void main(String[] args) {
+        // Declaration
+        
+        // int intArray[]; or 
+        int[] intArray; 
+        // byte byteArray[];
+        // short shortsArray[];
+        // boolean booleanArray[];
+        // long longArray[];
+        // float floatArray[];
+        // double doubleArray[];
+        // char charArray[];
+
+        // Initialization
+        intArray = new int[3];
+        intArray[0] = 22; 
+        intArray[1] = 24; 
+        intArray[2] = 26; 
+
+        // displaying an entire array by traversing
+        for(int ele : intArray){
+            System.out.print(ele+" ");
+        }
+
+        // one line declaration and initalization of array
+        int[] newArr = {1,2,3,4,5,6,7,8,9};
+        System.out.print("[ ");
+        for(int ele : newArr){
+            System.out.print(ele+", ");
+        }
+        System.out.print("]\n");
+
+        // array lenght property gives length of an array.
+        System.out.println("length = "+newArr.length); 
+    }
 }
