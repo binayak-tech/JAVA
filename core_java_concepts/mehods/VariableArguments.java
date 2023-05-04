f43569ed4c1b184e7c7f92c0358c4cfb4f051432
Binayak Purohit <binayakpurohit007@gmail.com>
2023-05-05 05:19:03 +0530

    varargs or variable arguments concept added

---
+++ core_java_concepts/mehods/VariableArguments.java
@@ -0,0 +1,20 @@
+package mehods;
+
+class VariableArguments{
+
+    static int sum(int ... arr){
+        // ... is used when the number of arguments are unknown
+        // this is storing all parameters into an array called arr
+        int result = 0;
+        for(int ele:arr){
+            result += ele;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum of 1,2 = " + sum(1,2));
+        System.out.println("Sum of 2,3,4 = " + sum(2,3,4));
+        System.out.println("Sum of 4,5,6,5,7,8,5,3 = " + sum(4,5,6,5,7,8,5,3));
+    }
+}
